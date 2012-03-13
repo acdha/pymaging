96e87f32efce2fcddd2151074d37e7193c7876d6
Chris Adams <chris@improbable.org>
2012-03-13 09:50:18 -0700

    Provide a simple fallback using stdlib
    This allows running the test suite without requiring numpy and vbench, which is
    not on PyPI
    
    * Reorganized benchmarks into separate files which can be run independently

--- bench/suite.py
+++ bench/suite.py
@@ -1,36 +1,58 @@
-from vbench.api import Benchmark, GitRepo
 from datetime import datetime
-
 import os
+import tempfile
+import timeit
+import sys
 
-setup = """
-from pymaging import Image
-"""
+try:
+    from vbench.api import Benchmark, GitRepo, BenchmarkRunner
+    HAS_VBENCH = True
+except ImportError:
+    HAS_VBENCH = False
 
-stmt = ("Image.open_from_path('testimage.png')."
-        "flip_left_right().save_to_path('benchimage.png')")
+PYMAGING_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+OUTPUT_DIR = tempfile.gettempdir()
 
-pymaging_benchmark = Benchmark(stmt, setup)
 
-benchmarks = [pymaging_benchmark]
+def discover_benchmarks():
+    for bench in os.listdir(os.path.join(PYMAGING_ROOT, "bench", "benchmarks")):
+        if not bench.endswith(".py") or bench.startswith("_"):
+            continue
+        yield "benchmark()", "from benchmarks.%s import benchmark" % os.path.splitext(bench)[0]
 
-HOME = os.path.expanduser('~')
-TMP_DIR = os.path.join(HOME, 'tmp/vb_pymaging')
-DB_PATH = os.path.join(HOME, 'code/repos/pymaging/bench/benchmarks.db')
-REPO_URL = '..'
-REPO_PATH = '..'
 
-START_DATE = datetime(2012, 2, 27)
-repo = GitRepo(REPO_PATH)
+def run_vbench():
+    START_DATE = datetime(2012, 2, 27)
+    DB_PATH = os.path.join(PYMAGING_ROOT, 'bench', 'benchmarks.db')
+    REPO_URL = '..'
 
-from vbench.api import BenchmarkRunner
+    repo = GitRepo(PYMAGING_ROOT)
 
-def run_process():
-    runner = BenchmarkRunner(benchmarks, REPO_PATH, REPO_URL,
-                             '', DB_PATH, TMP_DIR, '',
+    benchmarks = [Benchmark(stmt, setup) for stmt, setup in discover_benchmarks()]
+
+    runner = BenchmarkRunner(benchmarks, PYMAGING_ROOT, REPO_URL,
+                             '', DB_PATH, OUTPUT_DIR, '',
                              run_option='all', start_date=START_DATE,
                              module_dependencies=[])
     runner.run()
 
+
+def run_simple():
+    try:
+        repeats, number = map(int, sys.argv[1:2])
+    except:
+        repeats = 3
+        number = 5
+
+    for stmt, setup in discover_benchmarks():
+        times = timeit.repeat(stmt, setup, repeat=repeats, number=number)
+
+        print "Results after %s repeats of %d calls:" % (repeats, number)
+        print "Min: %0.3f" % min(times)
+        print "Max: %0.3f" % max(times)
+
 if __name__ == '__main__':
-    run_process()
+    if HAS_VBENCH:
+        run_vbench()
+    else:
+        run_simple()
